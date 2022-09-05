48a149b777819e2d1ccc9a7ed07af9a1e21785ed
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-09-05 20:24:02 +0300

    добавлены данные сотрудников для теста

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,40 @@
 public class Employee {
-    private String fullname;
+    private String fullName;
     private int department;
     private double salary;
+    private int id;
 
-    public Employee (String fullname, int department, double salary) {
-        this.fullname = fullname;
+    private static int counter = 0;
+
+
+
+    public Employee (String fullName, int department, double salary) {
+        this.fullName = fullName;
         this.department = department;
         this.salary = salary;
+        this.id = counter;
+        counter++;
+    }
+    public String getFullName () {
+        return this.fullName;
+    }
+    public int getDepartment () {
+        return  this.department;
+    }
+    public double getSalary() {
+        return this.salary;
+    }
+    public int getId () {
+        return this.id;
+    }
+    public void setDepartment (int department) {
+        this.department = department;
+    }
+    public  void setSalary (double salary) {
+        this.salary = salary;
+    }
+    @Override
+    public String toString () {
+        return this.id + ". " + this.fullName + ", Отдел № " + this.department + ", зароботная плата " + this.salary;
     }
 }

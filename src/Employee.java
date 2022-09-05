5d3982eeeef961000b6f7e338c2cdc82ca319897
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-09-05 20:00:11 +0300

    базовая сложность

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String fullname;
+    private int department;
+    private double salary;
+
+    public Employee (String fullname, int department, double salary) {
+        this.fullname = fullname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
